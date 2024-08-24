2e5240855abec2cb8e1ee9553ee82a3265e3c10d
ilan111 <ilanydg@gmail.com>
2024-08-24 12:35:21 +0300

    Changed duplicate code into one category component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 const App = () => {
-  return <div className="App"></div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <div className="categories-container">
+        {categories.map(({ title }) => (
+          <div className="catergory-container">
+            {/* <img src="" alt="" /> */}
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
